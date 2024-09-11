f2885997273ed6cda7896ae18232d15d368c5a64
Luana Marley <123222844+conectomarley@users.noreply.github.com>
2024-09-11 21:26:12 +1000

    update html

---
+++ index.js
@@ -0,0 +1,10 @@
+function showAnswer(response) {
+  alert(response.data.answer);
+}
+
+let apiKey = "3t4aaf46b1o5b1534f3e944eb00eb10d";
+let context = "provide a very short answer and make sure to pick one";
+let prompt = "who was the first female president in the world";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+console.log("apiUrl");
+axios.get(apiUrl).then(showAnswer);
